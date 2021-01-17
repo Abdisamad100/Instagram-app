0939653466bade8554a61390b214679f83aee838
abdisamad100 <abdisamadcade004@gmail.com>
2021-01-17 17:17:24 +0300

    modified models.py

--- instagram/insta/models.py
+++ instagram/insta/models.py
@@ -40,3 +40,90 @@ class Profile(models.Model):
     def filter_by_id(cls, id):
         profile = Profile.objects.filter(user = id).first()
         return profile
+
+
+
+class Image(models.Model):
+    name = models.CharField(max_length = 50)
+    picture = ImageField(blank = True, manual_crop = '1080x1080')
+    caption = HTMLField()
+    posted = models.DateTimeField(auto_now_add=True)
+    profile = models.ForeignKey(User, on_delete=models.CASCADE)
+    profile_det = models.ForeignKey(Profile, on_delete=models.CASCADE)
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
+    def is_liked(self):
+        pass
+
+        
+    @classmethod
+    def update_caption(self, caption):
+        update_img = cls.objects.filter(id = id).update(caption = caption)
+        return update_img
+
+    @classmethod
+    def get_all_images(cls):
+        images = cls.objects.all()
+        return images
+
+    @classmethod
+    def get_image_by_id(cls, id):
+        image = cls.objects.filter(id = id).all()
+        return image
+
+    @classmethod
+    def get_profile_pic(cls, profile):
+        images = Image.objects.filter(profile__pk = profile)
+        return images
+
+    @property
+    def count_comments(self):
+        comments = self.comments.count()
+        return comments
+
+    @property
+    def count_likes(self):
+        likes = self.likes.count()
+        return likes
+
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['posted']
+
+
+class Likes(models.Model):
+    image = models.ForeignKey(Image, on_delete=models.CASCADE, related_name='likes')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='likes')
+
+    def save_like(self):
+        self.save()
+
+    def unlike_like(self):
+        self.delete()
+
+
+    
+class Comments(models.Model):
+    comment = models.CharField(max_length = 300)
+    posted_on = models.DateTimeField(auto_now=True)
+    image = models.ForeignKey(Image, on_delete=models.CASCADE, related_name='comments')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def save_comment(self):
+        self.save()
+
+    def delete_comment(self):
+        self.delete()
+
+    @classmethod
+    def get_comments_by_images(cls, id):
+        comments = Comments.objects.filter(image__pk = id)
+        return comments        
